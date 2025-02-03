14892b9db4fe788a7d3483f09a20829c9a1a008e
DarkLightBegin23 <samseong0418@naver.com>
2025-02-03 10:33:29 +0900

    ~20250203 Java files

---
+++ hello_world/src/variable/Var6.java
@@ -0,0 +1,10 @@
+package variable;
+
+public class Var6 {
+    public static void main(String[] args) {
+        int a = 4;
+        boolean b = true;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
---
+++ hello_world/src/variable/Var8.java
@@ -0,0 +1,16 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        // 정수
+        // byte b = 127;  // -128 ~ 127
+        // short s = 32627; // -32768 ~ 32767
+        int i = 2; // -약 21억 ~ 20억
+
+        long l = 222L;
+
+        // 실수
+        // float f = 10.0f;
+        double d = 10.0;  // 주로 쓰는 거
+    }
+}
