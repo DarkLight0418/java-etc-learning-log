c097cf80e230d439b505a25d14d89980a3c482c7
DarkLightBegin23 <samseong0418@naver.com>
2025-02-05 13:41:53 +0900

    20250205

---
+++ hello_world/src/chapter5_class1/Student.java
@@ -0,0 +1,8 @@
+package chapter5_class1;
+
+public class Student {  // 클래스 이름 (class - 클래스를 만드는 예약어)
+    int studentID;
+    String studentName;
+    int grade;
+    String address;  // 상위 4개 멤버 변수
+}
