91ab6411ea56cfde0dac4c5be031fbdfbf41d54b
DarkLightBegin23 <samseong0418@naver.com>
2025-02-12 09:13:26 +0900

    20250211 class prac.

---
+++ hello_world/src/chapter5_class1/Student1.java
@@ -0,0 +1,10 @@
+package chapter5_class1;
+
+public class Student1 {
+    int studentID;
+    String studentName;
+    int koreaScore;
+    int mathScore;
+    String koreaSubject;
+    String mathSubject;
+}
---
+++ hello_world/src/chapter5_class1/Subject.java
@@ -0,0 +1,6 @@
+package chapter5_class1;
+
+public class Subject {
+    String SubjectName;
+    int scorePoint;
+}
