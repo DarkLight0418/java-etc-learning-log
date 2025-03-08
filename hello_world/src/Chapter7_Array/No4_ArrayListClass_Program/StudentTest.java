f9712b23c713d182b2a0ffa565be3602edf91da3
DarkLightBegin23 <samseong0418@naver.com>
2025-03-08 18:09:34 +0900

    20250308 arrayList

---
+++ hello_world/src/Chapter7_Array/No4_ArrayListClass_Program/StudentTest.java
@@ -0,0 +1,18 @@
+package Chapter7_Array.No4_ArrayListClass_Program;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        Student studentLee = new Student(1001, "Lee");
+        studentLee.addSubject("국어", 90);
+        studentLee.addSubject("수학", 80);
+
+        Student studentKim = new Student(1002, "Kim");
+        studentKim.addSubject("국어", 100);
+        studentKim.addSubject("수학",85);
+        studentKim.addSubject("영어",95);
+
+        studentLee.showStudentInfo();
+        System.out.println("====================================");
+        studentKim.showStudentInfo();
+    }
+}
