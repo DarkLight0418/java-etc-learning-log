616761d0013938dbadfa3038911ac9c2010fafbe
DarkLightBegin23 <samseong0418@naver.com>
2025-03-09 17:01:27 +0900

    20250309 First Baekjoon

---
+++ baekjoon/1000.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner plus = new Scanner(System.in);
+
+        int A = plus.nextInt();
+        int B = plus.nextInt();
+
+        int C = A + B;
+        System.out.println(C);
+    }
+}
---
+++ baekjoon/1001.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner plus = new Scanner(System.in);
+
+        int A = plus.nextInt();
+        int B = plus.nextInt();
+
+        System.out.println(A-B);
+    }
+}
