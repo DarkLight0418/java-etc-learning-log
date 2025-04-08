2ef0b8ddd01daabc353ad7d6d3a4897f41d28b5c
DarkLightBegin23 <samseong0418@naver.com>
2025-04-09 06:09:22 +0900

    20250409 String

---
+++ hello_world/src/Chapter11_JDKBasicClass/No1_Object/StringTest2.java
@@ -0,0 +1,15 @@
+package Chapter11_JDKBasicClass.No1_Object;
+
+public class StringTest2 {
+    public static void main(String[] args) {
+        String javaStr = new String("java");
+        String androidStr = new String("android");
+        System.out.println(javaStr);
+        System.out.println("처음 문자열 주솟값: " + System.identityHashCode(javaStr));
+
+        javaStr = javaStr.concat(androidStr);  // javaStr과 androidStr을 연결하여 javaStr에 대입
+
+        System.out.println(javaStr);
+        System.out.println("연결된 문자열 주솟값: " + System.identityHashCode(javaStr));
+    }
+}
