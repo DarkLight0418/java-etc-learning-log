bd4ab9aae06ecdd0c57c64919b02c26646ddee91
DarkLightBegin23 <samseong0418@naver.com>
2025-05-09 13:52:56 +0900

    20250509 HashSet

--- hello_world/src/Chapter12_CollectionFW/collection/Member.java
+++ hello_world/src/Chapter12_CollectionFW/collection/Member.java
@@ -1,16 +1,20 @@
 package Chapter12_CollectionFW.collection;
 
+import java.util.Objects;
+
 public class Member {
     private int memberID;  // 회원 아이디
-    private  String memberName;  // 회원 이름
+    private String memberName;  // 회원 이름
 
     public Member(int memberID, String memberName) {
         this.memberID = memberID;
         this.memberName = memberName;
     }
+
     public int getMemberID() {
         return memberID;
     }
+
     public void setMemberID(int memberID) {
         this.memberID = memberID;
     }
@@ -26,5 +30,22 @@ public class Member {
     @Override
     public String toString() {
         return memberName + " 회원님의 아이디는 " + memberID + "입니다.";
+    }
+
+    @Override
+    public int hashCode() {
+        return memberID; // hashCode() 메서드가 회원 아이디를 반환하도록 재정의
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Member) {
+            Member member = (Member) obj;
+            if (this.memberID == member.memberID)  // 매개변수로 받은 회원 아이디가 자신의 회원 아이디와 같다면 true 반환
+                return true;
+            else
+                return false;
+        }
+        return false;
     }
 }
