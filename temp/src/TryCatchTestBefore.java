4b34e541c23d20fd8fc3dbfc1752eb6dd095f79f
DarkLightBegin23 <samseong0418@naver.com>
2025-06-04 18:23:15 +0900

    20250604 try-catch test

---
+++ temp/src/TryCatchTestBefore.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TryCatchTest {
+    public static void SIUUU {
+        Scanner sc = new Scanner(System.in); // 스캐너 객체
+
+        System.out.println("a : ");
+        int a = sc.nextInt();
+        System.out.println("b : ");
+        int b = sc.nextInt();
+    }
+
+    public static void main(String[] args) {
+        try {
+            SIUUU siu = new SIUUU();
+        }
+        catch (Exception e){
+
+        }
+    }
+}
